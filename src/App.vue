4793abc811d2e112fe1945fd8d6851bcc52906a3
Poplavskij Vladimir <float45@gmail.com>
2020-06-02 10:38:32 +0300

    Fix image

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Brain" src="./assets/brain-2062057_640.jpg">
-    <caption>Image <a href="https://pixabay.com/ru/users/ElisaRiva-1348268">ElisaRiva</a> from stock <a href="https://pixabay.com/ru/">Pixabay</a></caption>
+    <figure>
+      <img alt="Brain with neural networks" src="./assets/brain-2062057_640.jpg" width="640" height="487"/>
+      <figcaption>Image <a href="https://pixabay.com/ru/users/ElisaRiva-1348268">ElisaRiva</a> from stock <a href="https://pixabay.com/ru/">Pixabay</a></figcaption>
+    </figure>
     <NeuralNetworks msg="Neural network analysis"/>
   </div>
 </template>
@@ -25,5 +27,28 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+a, figure {
+  display: inline-block;
+}
+figcaption {
+  margin: 10px 0 0 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+figure {
+  padding: 5px;
+}
+img:hover {
+  transform: scale(1.1);
+  -ms-transform: scale(1.1);
+  -webkit-transform: scale(1.1);
+  -moz-transform: scale(1.1);
+  -o-transform: scale(1.1);
+}
+img {
+  transition: transform 0.2s;
+  -webkit-transition: -webkit-transform 0.2s;
+  -moz-transition: -moz-transform 0.2s;
+  -o-transition: -o-transform 0.2s;
 }
 </style>
