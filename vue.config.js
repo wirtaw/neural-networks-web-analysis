41452b490e535b07fb161edcc4e5ed4ae4697570
Poplavskij Vladimir <float45@gmail.com>
2020-06-25 23:52:24 +0300

    Add predict form

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/neural_networks'
-    : '/'
+    : '/',
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "@/styles/index.scss";`
+      }
+    }
+  }
 }
