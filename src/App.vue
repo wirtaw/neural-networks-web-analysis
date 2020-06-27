a37820a07e197cb0d010da7ee953a37f15ee6e61
Poplavskij Vladimir <float45@gmail.com>
2020-06-27 21:17:48 +0300

    Fix build

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,56 @@
 <template>
-  <div id="app">
+  <div
+    id="app"
+    class="app"
+  >
     <router-view />
   </div>
 </template>
 
 <script>
+  // import 'bulma/css/bulma.css';
+  // import './styles/extensions/steps/bulma-steps.css';
+  // import './styles/extensions/switch/bulma-switch.css';
+  import './styles/extensions/steps/index';
 export default {
   name: 'App'
 }
 
 </script>
 
-<style lang="sass">
+<style lang="scss">
+  @import 'bulma/bulma.sass';
+  @import './styles/extensions/steps/bulma-steps.sass';
+  @import "./styles/extensions/switch/bulma-switch.sass";
+.app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 
+    a, figure {
+        display: inline-block;
+    }
+      figcaption {
+        margin: 10px 0 0 0;
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+    }
+      figure {
+        padding: 5px;
+    }
+      img:hover {
+        transform: scale(1.1);
+        -ms-transform: scale(1.1);
+        -webkit-transform: scale(1.1);
+        -moz-transform: scale(1.1);
+        -o-transform: scale(1.1);
+    }
+      img {
+        transition: transform 0.2s;
+        -webkit-transition: -webkit-transform 0.2s;
+        -moz-transition: -moz-transform 0.2s;
+        -o-transition: -o-transform 0.2s;
+    }
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,4 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/neural_networks'
     : '/',
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: `@import "@/styles/index.scss";`
-      }
-    }
-  }
 }
